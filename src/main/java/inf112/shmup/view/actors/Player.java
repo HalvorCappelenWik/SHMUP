b62bf96d4e8a57814e8fd0debfec978026f1ed1a
Lars Haukland <lars.haukland@gmail.com>
2023-03-05 20:19:55 +0100

    Made player use sprite and implemented very basic methods

--- src/main/java/inf112/shmup/view/actors/Player.java
+++ src/main/java/inf112/shmup/view/actors/Player.java
@@ -1,7 +1,43 @@
 package inf112.shmup.view.actors;
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 
 public class Player extends Actor{
+	Sprite sprite = new Sprite(new Texture(new FileHandle("src/assets/playerShip1_blue.png")));
+	
+	public Player() {
+		setBounds(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
+	}
+	
+	
+
+	@Override
+	protected void positionChanged() {
+		sprite.setPosition(getX(), getY());
+		super.positionChanged();
+	}
+
+
+
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		sprite.draw(batch);
+	}
+
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+	}
+	
+	
 
 }
