c7aa9d821371a2a022e85f444d9381eea2594324
Halvor Cappelen Wik <halvorcappelenwik@pc93-198.eduroam.uib.no>
2023-03-06 13:19:35 +0100

    moved playerlogic into player.java from gamescreen.java

--- src/main/java/inf112/shmup/view/actors/Player.java
+++ src/main/java/inf112/shmup/view/actors/Player.java
@@ -1,27 +1,30 @@
 package inf112.shmup.view.actors;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class Player extends Actor{
+public class Player extends Actor {
+
+	private final float _secondsBetweenBullets = 0.2f;
+	private float _secondsSinceLastBullet = 0f;
+
 	Sprite sprite = new Sprite(new Texture(new FileHandle("src/assets/playerShip1_blue.png")));
 	
 	public Player() {
 		setBounds(1, 1, sprite.getWidth(), sprite.getHeight());
 	}
-	
-	
 
 	@Override
 	protected void positionChanged() {
 		sprite.setPosition(getX(), getY());
 		super.positionChanged();
 	}
-
-
 
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
@@ -30,13 +33,39 @@ public class Player extends Actor{
 
 	@Override
 	public void act(float delta) {
-		super.act(delta);
+		// Input handling
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+			if(moveInBounds(-5f, 0)) 	this.moveBy(-5f, 0);
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			if(moveInBounds(5f, 0))	this.moveBy(5f, 0);
+		if(Gdx.input.isKeyPressed(Input.Keys.UP))
+			if(moveInBounds(0, 5f))	this.moveBy(0, 5f);
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+			if(moveInBounds(0, -5f))	this.moveBy(0, -5f);
+
+		// Spawn new bullet every 200th of a second
+		if(_secondsSinceLastBullet > _secondsBetweenBullets) {
+			PlayerBullet newBullet = new PlayerBullet(this.getX() + this.getWidth()/2, this.getY() + this.getHeight());
+			this.getStage().addActor(newBullet);
+			_secondsSinceLastBullet = 0f;
+		}
+		else {
+			_secondsSinceLastBullet += delta;
+		}
 	}
 	
 	public Sprite getSprite() {
 		return sprite;
 	}
-	
-	
 
+	private boolean moveInBounds(float x, float y) {
+		Rectangle gameBounds = new Rectangle(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Rectangle playerBounds = this.getSprite().getBoundingRectangle();
+		float newPlayerX = playerBounds.getX() + x;
+		float newPlayerY = playerBounds.getY() + y;
+		playerBounds.setX(newPlayerX);
+		playerBounds.setY(newPlayerY);
+
+		return gameBounds.contains(playerBounds);
+	}
 }
--- src/main/java/inf112/shmup/view/screens/GameScreen.java
+++ src/main/java/inf112/shmup/view/screens/GameScreen.java
@@ -20,9 +20,7 @@ public class GameScreen implements Screen{
 	private final ShmupGame game;
 	private Stage stage;
 	private Player player;
-	
-	private final float _secondsBetweenBullets = 0.2f;
-	private float _secondsSinceLastBullet = 0f;
+
 	
 	
 	public GameScreen(ShmupGame game) {
@@ -45,77 +43,34 @@ public class GameScreen implements Screen{
 	public void render(float delta) {
 		Gdx.gl.glClearColor(.25f, .25f, .25f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		update(delta);
-		
+
 		stage.act(delta);
 
 		stage.draw();
-		
-
-	}
-	
-	private void update(float dt) {
-		// Input handling
-		if(Gdx.input.isKeyPressed(Keys.LEFT))
-			if(moveInBounds(-5f, 0)) 	player.moveBy(-5f, 0);
-		if(Gdx.input.isKeyPressed(Keys.RIGHT))
-			if(moveInBounds(5f, 0))	player.moveBy(5f, 0);
-		if(Gdx.input.isKeyPressed(Keys.UP))
-			if(moveInBounds(0, 5f))	player.moveBy(0, 5f);
-		if(Gdx.input.isKeyPressed(Keys.DOWN))
-			if(moveInBounds(0, -5f))	player.moveBy(0, -5f);
-		
-		// Spawn new bullet every 200th of a second
-		if(_secondsSinceLastBullet > _secondsBetweenBullets) {
-			PlayerBullet newBullet = new PlayerBullet(player.getX() + player.getWidth()/2, player.getY() + player.getHeight());
-			stage.addActor(newBullet);
-			_secondsSinceLastBullet = 0f;
-		}
-		else {
-			_secondsSinceLastBullet += dt;
-		}
-	}
-	
-	private boolean moveInBounds(float x, float y) {
-		Rectangle gameBounds = new Rectangle(0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Rectangle playerBounds = player.getSprite().getBoundingRectangle();
-		float newPlayerX = playerBounds.getX() + x;
-		float newPlayerY = playerBounds.getY() + y;
-		playerBounds.setX(newPlayerX);
-		playerBounds.setY(newPlayerY);
-		
-		return gameBounds.contains(playerBounds);
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void dispose() {
 		stage.dispose();
-		
 	}
-
 }
