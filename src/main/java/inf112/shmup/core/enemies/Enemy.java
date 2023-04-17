15fa94bc75ef1c36e26b5ca893640c5a28a523b5
Philip.Svenningsen <psv007@uib.no>
2023-04-17 15:38:43 +0200

    working enemy fly-in #45

--- src/main/java/inf112/shmup/core/enemies/Enemy.java
+++ src/main/java/inf112/shmup/core/enemies/Enemy.java
@@ -2,7 +2,14 @@ package inf112.shmup.core.enemies;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.utils.Align;
+
+import org.w3c.dom.css.Rect;
 
 import inf112.shmup.core.Game;
 import inf112.shmup.core.bullets.Bullet;
@@ -17,6 +24,7 @@ import inf112.shmup.core.utilities.ScoreManager;
 public class Enemy extends DrawableActor implements Damageable { //might be a good idea to make an abstract class if we want enemy variety
 
     public boolean killed = false;
+	private boolean active = false;
 
     int dir = -1;
     float speed = 100;
@@ -27,8 +35,17 @@ public class Enemy extends DrawableActor implements Damageable { //might be a go
     public Enemy(float x, float y){
         
         setSprite(AssetManager.sprite("ships/ship_red1.png"));
-		setPosition(x, y, Align.center);
+		setPosition(x, y + Game.V_HEIGHT);
 		setEnemyRotation(sprite);
+		
+		//implement into abstract enemy instead
+		//make a static sequence
+		Action sequence = new SequenceAction(
+											Actions.moveTo(x, y+25, 2.2f), 
+											Actions.moveTo(x, y+5, 0.2f),
+											Actions.moveTo(x, y, 0.4f));
+											
+		this.addAction(sequence);
     }
 
 // ------------------------------- Override actor methods -----------------------------------
@@ -36,7 +53,13 @@ public class Enemy extends DrawableActor implements Damageable { //might be a go
     @Override
     public void act(float delta) {
 		
-    	if(killed) return;
+		super.act(delta);
+
+		if(getActions().size <= 0){
+			active = true;
+		}
+    	
+		if(killed || !active) return;
         //move
         this.moveBy(dir * speed * delta, 0);
 		Rectangle bounds = new Rectangle(0, 0, Game.V_WIDTH, Game.V_HEIGHT);
@@ -75,10 +98,24 @@ public class Enemy extends DrawableActor implements Damageable { //might be a go
 	private void moveIntoBounds(float x, float y) {
 		Rectangle gameBounds = new Rectangle(0,0, Game.V_WIDTH, Game.V_WIDTH);
 	
-		setX(Math.max(gameBounds.x, getX()));
+		setX(clampX(getX(), gameBounds));
 		setX(Math.min(gameBounds.x + gameBounds.width - getWidth(), getX()));
-		setY(Math.max(gameBounds.y, getY()));
+		setY(clampY(getY(), gameBounds));
 		setY(Math.min(gameBounds.y + gameBounds.height - getHeight(), getY()));
+	}
+
+	private float clampX(float x, Rectangle bounds){
+		
+		x = Math.max(bounds.x,x);
+		x = Math.min(bounds.x + bounds.width - getWidth(), x);
+		return x;
+	}
+
+	private float clampY(float y, Rectangle bounds){
+
+		y = Math.max(bounds.y, y);
+		y = Math.min(bounds.y + bounds.height - getHeight(), y);
+		return y;
 	}
 
 	@Override
