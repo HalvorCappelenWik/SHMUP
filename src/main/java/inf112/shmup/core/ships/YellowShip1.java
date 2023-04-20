af2b14feb8fd1b8c7194d96b63f6b8548670a89b
Ljubomir Simic <lsi020@uib.no>
2023-04-20 20:08:05 +0200

    Yellow Ship implemented

---
+++ src/main/java/inf112/shmup/core/ships/YellowShip1.java
@@ -0,0 +1,30 @@
+package inf112.shmup.core.ships;
+
+import inf112.shmup.core.bullets.PlayerBullet;
+import inf112.shmup.core.utilities.AssetManager;
+import inf112.shmup.core.utilities.AudioPlayer;
+
+public class YellowShip1 extends Ship {
+
+    public YellowShip1() {
+        super(AssetManager.sprite("ships/ship_yellow1.png"), 4);
+
+        addRifle(0.65f, () -> new PlayerBullet(1))
+                .useSound("shoot_1");
+    }
+
+    @Override
+    protected void onHealed() {
+        AudioPlayer.playEffect("player_killed");
+    }
+
+    @Override
+    protected void onDamaged() {
+
+    }
+
+    @Override
+    protected void onDead() {
+
+    }
+}
