5f364002c32617c6f05c49eb5e80d87eee49a54a
Lars Haukland <lars.haukland@gmail.com>
2023-04-20 23:16:28 +0200

    Zig1 added

--- src/main/java/inf112/shmup/core/enemies/Zig1.java
+++ src/main/java/inf112/shmup/core/enemies/Zig1.java
@@ -5,17 +5,21 @@ import inf112.shmup.core.Game;
 import inf112.shmup.core.utilities.AssetManager;
 
 public class Zig1 extends EnemyShip{
+	private int _dir = 1;
+	private float _angle = 260f;
 
 	public Zig1(float x, float y) {
 		super(x, y, AssetManager.sprite("ships/ship_9.png"), Integer.MAX_VALUE);
-		setSpeedX(100f);
-		setSpeedY(-50f);
+		
+		float speed = 150f;
+		setSpeedX((float)Math.sin(Math.toRadians(_angle)) * speed * -1);
+		setSpeedY((float)Math.cos(Math.toRadians(_angle)) * speed);
+		setRotation(_angle);
 	}
 
 	@Override
 	protected void onHealed() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -31,15 +35,19 @@ public class Zig1 extends EnemyShip{
 
 	@Override
 	public void act(float delta) {
-		// TODO Auto-generated method stub
 		super.act(delta);
 		
-		moveBy(getSpeedX() * delta, getSpeedY() * delta);
+		moveBy(getSpeedX() * delta * _dir, getSpeedY() * delta);
 		
-		if(getX() >= SCREEN_BOUNDS.width || getX() <= 0) {
-			setSpeedX(-1 * getSpeedX());
-			clampToBounds(SCREEN_BOUNDS);
-		}
+		if (getX() < 0) {
+            _dir = 1;
+            setRotation(_angle);
+        } else if (getX() + getWidth() > Game.V_WIDTH) {
+            _dir = -1;
+            setRotation(-_angle);
+        }
+        
+        if(getY() <= 0) remove();
 	}
 	
 	
