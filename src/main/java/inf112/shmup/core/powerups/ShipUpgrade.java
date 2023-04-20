624d338ab06d330a0d97a650b5deb203e6b9736b
Herman Stautland <herman.t.stautland@gmail.com>
2023-04-20 19:55:39 +0200

    Finished GunshipSpeed

--- src/main/java/inf112/shmup/core/powerups/ShipUpgrade.java
+++ src/main/java/inf112/shmup/core/powerups/ShipUpgrade.java
@@ -4,6 +4,9 @@ import inf112.shmup.core.Player;
 import inf112.shmup.core.ships.Gunship1;
 import inf112.shmup.core.ships.Gunship2;
 import inf112.shmup.core.ships.Gunship3;
+import inf112.shmup.core.ships.GunshipSpeed1;
+import inf112.shmup.core.ships.GunshipSpeed2;
+import inf112.shmup.core.ships.GunshipSpeed3;
 import inf112.shmup.core.ships.Ship;
 import inf112.shmup.core.utilities.AssetManager;
 import inf112.shmup.core.utilities.AudioPlayer;
@@ -11,7 +14,7 @@ import inf112.shmup.core.utilities.AudioPlayer;
 public class ShipUpgrade extends PowerUp {
     public static final String GUNSHIP_TYPE = "blue";
     public static final String NOT_IN_USE_1_TYPE = "yellow";
-    public static final String NOT_IN_USE_2_TYPE = "green";
+    public static final String SPEED_TYPE = "green";
     public static final String NOT_IN_USE_3_TYPE = "red";
 
     private final String _type;
@@ -32,8 +35,8 @@ public class ShipUpgrade extends PowerUp {
         else if (NOT_IN_USE_1_TYPE.equals(_type)) {
             
         }
-        else if (NOT_IN_USE_2_TYPE.equals(_type)) {
-            
+        else if (SPEED_TYPE.equals(_type)) {
+            upgradeGunshipSpeed();
         }
         else if (NOT_IN_USE_3_TYPE.equals(_type)) {
             
@@ -58,6 +61,22 @@ public class ShipUpgrade extends PowerUp {
         else {
             // The player had another ship-type.
             setShip(new Gunship1());
+        }
+    }
+
+    private void upgradeGunshipSpeed() {
+        if (currentShip() instanceof GunshipSpeed1) {
+            setShip(new GunshipSpeed2());
+        }
+        else if (currentShip() instanceof GunshipSpeed2) {
+            setShip(new GunshipSpeed3());
+        }
+        else if (currentShip() instanceof GunshipSpeed3) {
+            // The ship is upgraded to the maximum.
+        }
+        else {
+            // The player had another ship-type.
+            setShip(new GunshipSpeed1());
         }
     }
 
